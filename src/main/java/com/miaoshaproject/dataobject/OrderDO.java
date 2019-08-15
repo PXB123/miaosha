a4da0949b04041bf4a733caf705be1dcd35afc89
pu_xb <2679862469@qq.com>
2019-08-15 16:12:16 +0800

    完成商品列表与详情

---
+++ src/main/java/com/miaoshaproject/dataobject/OrderDO.java
@@ -0,0 +1,63 @@
+package com.miaoshaproject.dataobject;
+
+public class OrderDO {
+    private String id;
+
+    private Integer userId;
+
+    private Integer itemId;
+
+    private Double itemPrice;
+
+    private Integer amout;
+
+    private Double orderPrice;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id == null ? null : id.trim();
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(Integer itemId) {
+        this.itemId = itemId;
+    }
+
+    public Double getItemPrice() {
+        return itemPrice;
+    }
+
+    public void setItemPrice(Double itemPrice) {
+        this.itemPrice = itemPrice;
+    }
+
+    public Integer getAmout() {
+        return amout;
+    }
+
+    public void setAmout(Integer amout) {
+        this.amout = amout;
+    }
+
+    public Double getOrderPrice() {
+        return orderPrice;
+    }
+
+    public void setOrderPrice(Double orderPrice) {
+        this.orderPrice = orderPrice;
+    }
+}
